525f5ea12b969100bc77715a6a3f82bb95d3f0a8
Tayon <tayondw@gmail.com>
2024-09-03 10:01:03 -0400

    updated event and group delete modal

--- react-vite/src/components/Events/CRUD/Delete/DeleteEvent.jsx
+++ react-vite/src/components/Events/CRUD/Delete/DeleteEvent.jsx
@@ -3,52 +3,50 @@ import { useModal } from "../../../../context/Modal";
 import "./DeleteEvent.css";
 
 const DeleteEvent = ({ eventDetails }) => {
-      const { closeModal } = useModal();
+	const { closeModal } = useModal();
 	const onClose = async (event) => {
 		event.preventDefault();
 		closeModal();
 	};
 
 	return (
-		<div id="deleteMenu">
+		<div id="event-deleteMenu">
 			<div id="event-close-confirm">
-				<h1>Confirm Delete</h1>
-				<h3>Are you sure you want to remove this event?</h3>
 				<button id="delete-close-button" onClick={onClose}>
 					✖
 				</button>
+				<div id="confirm-delete">
+					<h1>Confirm Delete</h1>
+					<h3>Are you sure you want to remove {eventDetails.name}?</h3>
+				</div>
 			</div>
 			<div id="delete-event">
-				<div>
-					<h2>Are you sure you want to delete {eventDetails.name}?</h2>
-				</div>
-				<div id="event-delete-buttons">
-					<Form
-						method="delete"
-						action={`/events/${eventDetails.id}`}
-						onSubmit={closeModal}
+				<Form
+					method="delete"
+					action={`/events/${eventDetails.id}`}
+					onSubmit={closeModal}
+				>
+					<button
+						type="submit"
+						name="intent"
+						style={{ backgroundColor: "red" }}
+						value="delete-event"
+						id="button-text"
+						className="event-delete-button-modal"
 					>
-						<button
-							type="submit"
-							name="intent"
-							style={{ backgroundColor: "red" }}
-							value="delete-event"
-							className="event-delete-button"
-						>
-							Yes (Delete Event)
-						</button>
-						<button
-							id="button-text"
-							style={{ backgroundColor: "darkgray" }}
-							onClick={onClose}
-						>
-							No (Keep Event)
-						</button>
-						<input type="hidden" name="group_id" value={eventDetails.groupId} />
-						<input type="hidden" name="eventId" value={eventDetails.id} />
-						<input type="hidden" name="id" value={eventDetails.id} />
-					</Form>
-				</div>
+						Yes (Delete Event)
+					</button>
+					<button
+						id="button-text"
+						style={{ backgroundColor: "darkgray" }}
+						onClick={onClose}
+					>
+						No (Keep Event)
+					</button>
+					<input type="hidden" name="group_id" value={eventDetails.groupId} />
+					<input type="hidden" name="eventId" value={eventDetails.id} />
+					<input type="hidden" name="id" value={eventDetails.id} />
+				</Form>
 			</div>
 		</div>
 	);
