7e062906f96e3fa5034242f5e733a955ac4bc372
Tayon <tayondw@gmail.com>
2024-09-17 12:19:10 -0400

    updated signup form to include the user info

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, abort, redirect, session
-from app.models import User, db
+from app.models import User, db, Tag
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -14,20 +14,20 @@ import google.auth.transport.requests
 from tempfile import NamedTemporaryFile
 import json
 
-auth_routes = Blueprint('auth', __name__)
+auth_routes = Blueprint("auth", __name__)
 
 
-@auth_routes.route('/')
+@auth_routes.route("/")
 def authenticate():
     """
     Authenticates a user.
     """
     if current_user.is_authenticated:
         return current_user.to_dict()
-    return {'errors': {'message': 'Unauthorized'}}, 401
+    return {"errors": {"message": "Unauthorized"}}, 401
 
 
-@auth_routes.route('/login', methods=['POST'])
+@auth_routes.route("/login", methods=["POST"])
 def login():
     """
     Logs a user in
@@ -35,37 +35,67 @@ def login():
     form = LoginForm()
     # Get the csrf_token from the request cookie and put it into the
     # form manually to validate_on_submit can be used
-    form['csrf_token'].data = request.cookies['csrf_token']
+    form["csrf_token"].data = request.cookies["csrf_token"]
     if form.validate_on_submit():
         # Add the user to the session, we are logged in!
-        user = User.query.filter(User.email == form.data['email']).first()
+        user = User.query.filter(User.email == form.data["email"]).first()
         login_user(user)
         return user.to_dict()
     return form.errors, 401
 
 
-@auth_routes.route('/logout')
+@auth_routes.route("/logout")
 def logout():
     """
     Logs a user out
     """
     logout_user()
-    return {'message': 'User logged out'}
+    return {"message": "User logged out"}
 
 
-@auth_routes.route('/signup', methods=['POST'])
+@auth_routes.route("/signup", methods=["POST"])
 def sign_up():
     """
     Creates a new user and logs them in
     """
     form = SignUpForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
+    form["csrf_token"].data = request.cookies["csrf_token"]
     if form.validate_on_submit():
+        profile_image = form.profileImage.data
+
+        if not profile_image:
+            return {"message": "An image is required to create a profile."}, 400
+
+        try:
+            profile_image.filename = get_unique_filename(profile_image.filename)
+            upload = upload_file_to_s3(profile_image)
+        except Exception as e:
+            return {"message": f"Image upload failed: {str(e)}"}, 500
+
+        if "url" not in upload:
+            return {
+                "message": upload.get(
+                    "errors", "Image upload failed. Please try again."
+                )
+            }, 400
+
+        url = upload["url"]
+
         user = User(
-            username=form.data['username'],
-            email=form.data['email'],
-            password=form.data['password']
+            first_name=form.data["firstName"],
+            last_name=form.data["lastName"],
+            username=form.data["username"],
+            email=form.data["email"],
+            password=form.data["password"],
+            bio=form.data["bio"],
+            profile_image_url=url,
         )
+        
+        # Update the user's tags
+        selected_tags = form.userTags.data  # This returns a list of selected tags
+        tags_to_add = Tag.query.filter(Tag.name.in_(selected_tags)).all()
+        user.users_tags = tags_to_add
+        
         db.session.add(user)
         db.session.commit()
         login_user(user)
@@ -73,9 +103,9 @@ def sign_up():
     return form.errors, 401
 
 
-@auth_routes.route('/unauthorized')
+@auth_routes.route("/unauthorized")
 def unauthorized():
     """
     Returns unauthorized JSON when flask-login authentication fails
     """
-    return {'errors': {'message': 'Unauthorized'}}, 401
+    return {"errors": {"message": "Unauthorized"}}, 401
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,10 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField
+from wtforms import (
+    StringField,
+    PasswordField,
+    EmailField,
+    TextAreaField,
+    SelectMultipleField,
+)
 from flask_wtf.file import FileAllowed, FileField, FileRequired
 from wtforms.validators import DataRequired, ValidationError, Length
 from app.models import User
 
 ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
+
+tags = [
+    ("ANGER", "ANGER"),
+    ("ANXIETY", "ANXIETY"),
+    ("DEPRESSION", "DEPRESSION"),
+    ("SUBSTANCE ABUSE", "SUBSTANCE ABUSE"),
+    ("STRESS", "STRESS"),
+    ("TRAUMA", "TRAUMA"),
+    ("RELATIONSHIPS", "RELATIONSHIPS"),
+    ("GRIEF", "GRIEF"),
+    ("COMING OUT", "COMING OUT"),
+    ("SUICIDAL THOUGHTS", "SUICIDAL THOUGHTS"),
+]
 
 
 def user_exists(form, field):
@@ -24,6 +43,24 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
+    firstName = StringField(
+        "first name",
+        validators=[
+            DataRequired(),
+            Length(
+                min=3, max=20, message="First name must be between 3 and 20 characters"
+            ),
+        ],
+    )
+    lastName = StringField(
+        "last name",
+        validators=[
+            DataRequired(),
+            Length(
+                min=3, max=20, message="Last name must be between 3 and 20 characters"
+            ),
+        ],
+    )
     username = StringField(
         "username",
         validators=[
@@ -36,3 +73,24 @@ class SignUpForm(FlaskForm):
     )
     email = EmailField("email", validators=[DataRequired(), user_exists])
     password = PasswordField("password", validators=[DataRequired()])
+    bio = TextAreaField(
+        "Bio",
+        validators=[
+            DataRequired(),
+            Length(
+                min=50,
+                max=500,
+                message="Please enter at least 50 characters describing yourself",
+            ),
+        ],
+    )
+    profileImage = FileField(
+        "Profile Image",
+        validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))],
+    )
+    userTags = SelectMultipleField(
+        "Tags",
+        choices=tags,
+        validators=[DataRequired()],
+        coerce=str,
+    )
