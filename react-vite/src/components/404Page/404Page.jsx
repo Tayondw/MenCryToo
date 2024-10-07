3b333f38fa58fca6a6ed879e73c28b5fdfd0f8e6
Tayon <tayondw@gmail.com>
2024-10-07 12:45:52 -0400

    created a custom 404 page

--- react-vite/src/components/404Page/404Page.jsx
+++ react-vite/src/components/404Page/404Page.jsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
 import "./404Page.css";
 
 const FourZeroFourPage = () => {
 	return (
-		<div>
-			<h1>404 Page not found</h1>
-			<p>
+		<div id="error-page">
+			<h1 id="error-page-header">404 Page not found</h1>
+			<p id="error-page-p">
 				Not all those who wander are lost, but it seems you may have taken a
 				wrong turn.
 			</p>
+			<Link to="/">
+				<button id="update-group-cancel">HOME</button>
+			</Link>
 		</div>
 	);
 };
