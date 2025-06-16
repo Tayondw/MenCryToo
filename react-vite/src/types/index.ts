654ae65aebde05792c5fc247c820a9dcf1051f0a
Tayon <tayondw@gmail.com>
2025-06-16 13:31:45 -0400

    created a types file for users, groups, events, posts, comments, tags, etc

---
+++ react-vite/src/types/index.ts
@@ -0,0 +1,89 @@
+// User types
+export interface User {
+	id: number;
+	username: string;
+	firstName: string;
+	lastName: string;
+	email: string;
+	bio: string;
+	profileImage: string;
+	usersTags: Tag[];
+	posts: Post[];
+	group: Group[];
+	events: Event[];
+	userComments: Comment[];
+}
+
+export interface Tag {
+	id: number;
+	name: string;
+}
+
+export interface Post {
+	id: number;
+	title: string;
+	caption: string;
+	image: string;
+	likes: number;
+	updatedAt: string;
+}
+
+export interface Group {
+	id: number;
+	name: string;
+	about: string;
+	image: string;
+	numMembers: number;
+	events: Event[];
+	type: string;
+}
+
+export interface Event {
+	id: number;
+	name: string;
+	description: string;
+	image: string;
+	numAttendees: number;
+	capacity: number;
+	type: string;
+	startDate: string;
+	endDate: string;
+}
+
+export interface Comment {
+	id: number;
+	content: string;
+}
+
+// Redux types
+export interface RootState {
+	session: SessionState;
+}
+
+export interface SessionState {
+	user: User | null;
+}
+
+// Action types
+export interface SetUserAction {
+	type: "session/setUser";
+	payload: User;
+}
+
+export interface RemoveUserAction {
+	type: "session/removeUser";
+}
+
+export type SessionAction = SetUserAction | RemoveUserAction;
+
+// Thunk types - Simplified to work properly with redux-thunk
+export type AppThunk<ReturnType = void> = (
+	dispatch: AppDispatch,
+	getState: () => RootState,
+) => Promise<ReturnType> | ReturnType;
+
+// Simplified AppDispatch that works with useDispatch
+export interface AppDispatch {
+	<T extends SessionAction>(action: T): T;
+	<R>(asyncAction: AppThunk<R>): Promise<R>;
+}
