af9dc1fc3f4cd0e59c829ac13a0334dc5e5d5fcf
Tayon <tayondw@gmail.com>
2025-06-15 21:40:08 -0400

    updated to include optimizeDeps to exclude lucide-react

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -4,17 +4,20 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig((mode) => ({
-  plugins: [
-    react(),
-    eslintPlugin({
-      lintOnStart: true,
-      failOnError: mode === "production",
-    }),
-  ],
-  server: {
-    open: true,
-    proxy: {
-      "/api": "http://127.0.0.1:8000",
-    },
-  },
+	plugins: [
+		react(),
+		eslintPlugin({
+			lintOnStart: true,
+			failOnError: mode === "production",
+		}),
+	],
+	optimizeDeps: {
+		exclude: ["lucide-react"],
+	},
+	server: {
+		open: true,
+		proxy: {
+			"/api": "http://127.0.0.1:8000",
+		},
+	},
 }));
