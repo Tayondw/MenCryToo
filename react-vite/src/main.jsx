e1ef6d450c8f46266481765f9187af2672371b8c
Tayon <tayondw@gmail.com>
2025-06-20 17:27:47 -0400

    changed css import back to index.css

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import configureStore from "./redux/store";
 import { router } from "./router";
 import * as sessionActions from "./redux/session";
-import "./index2.css";
+import "./index.css";
 
 const store = configureStore();
 
