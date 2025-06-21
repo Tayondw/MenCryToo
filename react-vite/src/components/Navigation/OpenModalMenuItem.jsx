e3b8cf86b2eb73756bb25610d2af45b048b7838e
Tayon <tayondw@gmail.com>
2025-06-21 19:58:12 -0400

    updated Modal context import

--- react-vite/src/components/Navigation/OpenModalMenuItem.jsx
+++ react-vite/src/components/Navigation/OpenModalMenuItem.jsx
@@ -1,12 +1,12 @@
-import { useModal } from "../../context/Modal";
+import { useModal } from "../../context-TSX/Modal";
 
 function OpenModalMenuItem({
 	modalComponent, // component to render inside the modal
 	itemText, // text of the button that opens the modal
 	onItemClick, // optional: callback function that will be called once the button that opens the modal is clicked
 	onModalClose, // optional: callback function that will be called once the modal is closed
-      className,
-      style,
+	className,
+	style,
 }) {
 	const { setModalContent, setOnModalClose } = useModal();
 
@@ -17,7 +17,7 @@ function OpenModalMenuItem({
 	};
 
 	return (
-            <p style={style} className={className} onClick={onClick}>
+		<p style={style} className={className} onClick={onClick}>
 			{itemText}
 		</p>
 	);
