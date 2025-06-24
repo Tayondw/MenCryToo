22fb3e14809b740872ee97be16b166cd7eaca8c5
Tayon <tayondw@gmail.com>
2025-06-24 14:52:09 -0400

    using new store and session from store folder

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import configureStore from "./redux/store";
+import configureStore from "./store/store";
 import { router } from "./router";
-import * as sessionActions from "./redux/session";
+import * as sessionActions from "./store/session";
 import "./index.css";
 
 const store = configureStore();
