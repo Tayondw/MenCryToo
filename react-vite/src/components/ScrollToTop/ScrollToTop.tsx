95f3f3560e515685d60772161cda62b2fee2e426
Tayon <tayondw@gmail.com>
2025-07-02 16:31:34 -0400

    created ts version and removed js version

--- react-vite/src/components/ScrollToTop/ScrollToTop.tsx
+++ react-vite/src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const ScrollToTop = () => {
+const ScrollToTop: React.FC = () => {
 	const { pathname } = useLocation();
 
 	useEffect(() => {
@@ -9,6 +9,6 @@ const ScrollToTop = () => {
 	}, [pathname]);
 
 	return null;
-}
+};
 
 export default ScrollToTop;
