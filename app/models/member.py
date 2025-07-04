412de89060f9622bbd3ca0028673c7763d8431c0
Tayon <tayondw@gmail.com>
2025-07-04 15:42:19 -0400

    removed old commented out code

--- app/models/member.py
+++ app/models/member.py
@@ -26,23 +26,3 @@ class Membership(db.Model):
             "user": self.user.to_dict_no_posts() if self.user else None,
         }
 
-
-# members = db.Table(
-#     "members",
-#     db.Model.metadata,
-#     db.Column(
-#         "user_id",
-#         db.Integer,
-#         db.ForeignKey(add_prefix_for_prod("users.id")),
-#         primary_key=True,
-#     ),
-#     db.Column(
-#         "group_id",
-#         db.Integer,
-#         db.ForeignKey(add_prefix_for_prod("groups.id")),
-#         primary_key=True,
-#     ),
-# )
-
-# if environment == "production":
-#     members.schema = SCHEMA
