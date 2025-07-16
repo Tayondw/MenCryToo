55985fa1ffc64f8fb558476693396b03e100c224
Tayon <tayondw@gmail.com>
2025-07-15 22:55:16 -0400

    moved interfaces to types folder

--- react-vite/src/components/SuspenseWrapper/SuspenseWrapper.tsx
+++ react-vite/src/components/SuspenseWrapper/SuspenseWrapper.tsx
@@ -1,9 +1,6 @@
 import React, { Suspense } from "react";
 import PageLoader from "../PageLoader/PageLoader";
-
-interface SuspenseWrapperProps {
-	children: React.ReactNode;
-}
+import { SuspenseWrapperProps } from "../../types";
 
 const SuspenseWrapper: React.FC<SuspenseWrapperProps> = ({ children }) => (
 	<Suspense fallback={<PageLoader />}>{children}</Suspense>
