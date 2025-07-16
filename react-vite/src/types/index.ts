78e923c2c48f8c85ebaccb0b78684acceb2eafd5
Tayon <tayondw@gmail.com>
2025-07-15 22:53:49 -0400

    moved interfaces to types folder

--- react-vite/src/types/index.ts
+++ react-vite/src/types/index.ts
@@ -1,42 +1,22 @@
+export * from "./cache";
 export * from "./comments";
-import { Group } from "./groups";
-import { Event } from "./events";
-
-// User types
-export interface User {
-	id: number;
-	username: string;
-	firstName: string;
-	lastName: string;
-	email: string;
-	hashedPassword?: string; // Optional since it shouldn't be sent to frontend (this is for mock user)
-	bio: string;
-	profileImage: string;
-	usersTags: Tag[];
-	posts: Post[];
-	group: Group[];
-	events: Event[];
-	userComments: UserComment[]; // Renamed to avoid conflict with Comment interface
-	createdAt?: string;
-	updatedAt?: string;
-}
-export interface Tag {
-	id: number;
-	name: string;
-}
-
-export interface Post {
-	id: number;
-	title: string;
-	caption: string;
-	image: string;
-	likes: number;
-	creator: number;
-	comments: number; // Changed from string to number for count
-	createdAt: string;
-	updatedAt: string;
-	user: User;
-}
+export * from "./errors";
+export * from "./events";
+export * from "./filters";
+export * from "./groups";
+export * from "./hearts";
+export * from "./home";
+export * from "./login";
+export * from "./modal";
+export * from "./navigation";
+export * from "./posts";
+export * from "./private";
+export * from "./profile";
+export * from "./signup";
+export * from "./state";
+export * from "./suspense";
+export * from "./tags";
+export * from "./users";
 
 export interface Venue {
 	id: number;
@@ -46,52 +26,4 @@ export interface Venue {
 	state: string;
 	latitude: number;
 	longitude: number;
-}
-
-// Basic comment interface (for backwards compatibility) - renamed to avoid conflict
-export interface UserComment {
-	id: number;
-	comment: string;
-	userId?: number;
-	postId?: number;
-	createdAt?: string;
-	updatedAt?: string;
-	parentId?: number | null;
-	username?: string;
-}
-export interface ProfilesData {
-	users_profile: User[];
-}
-
-// Redux types
-export interface RootState {
-	session: SessionState;
-}
-
-export interface SessionState {
-	user: User | null;
-}
-
-// Action types
-export interface SetUserAction {
-	type: "session/setUser";
-	payload: User;
-}
-
-export interface RemoveUserAction {
-	type: "session/removeUser";
-}
-
-export type SessionAction = SetUserAction | RemoveUserAction;
-
-// Thunk types - Simplified to work properly with redux-thunk
-export type AppThunk<ReturnType = void> = (
-	dispatch: AppDispatch,
-	getState: () => RootState,
-) => Promise<ReturnType> | ReturnType;
-
-// Simplified AppDispatch that works with useDispatch
-export interface AppDispatch {
-	<T extends SessionAction>(action: T): T;
-	<R>(asyncAction: AppThunk<R>): Promise<R>;
 }
